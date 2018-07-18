38f6fad7d7d953f54fecc9361421cd850e806a3f
hhaller <fdytys@email.com>
2018-07-17 21:09:07 -0500

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,29 @@
 class UsersController < ApplicationController
-  def new
+
+  def show
+    @user = User.find(params[:id])
+    # debugger
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # handle a succesfull save
+      flash[:success] = "Bienvenido a Sampan"
+      redirect_to @user # equivalent (rails infers): redirect_to user_url(@user)
+    else
+      render 'new'
+    end
+  end
+
+private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
 end
