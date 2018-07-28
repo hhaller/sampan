db0e6ae7689eb28b3c9cf24444ef254e82828ffc
hhaller <fdytys@email.com>
2018-07-27 19:28:03 -0500

    fix jquery fail

--- Gemfile
+++ Gemfile
@@ -57,6 +57,7 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 # CSS styling
 gem 'bootstrap-sass'
 
+# para...
 gem 'rails-controller-testing'
 
 # Used by has_secure_password
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -16,12 +16,11 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
 
   test "valid login" do
     get login_path
-    post login_path, params: { session: { email: @user.email, password: 'password' }}
+    post login_path, params: { session: { email: @user.email, password: 'password' } }
     assert is_logged_in?
     assert_redirected_to @user
     follow_redirect!
     assert_template 'users/show'
-    assert is_logged_in?
     assert_select "a[href=?]", login_path, count: 0
     assert_select "a[href=?]", logout_path
     assert_select "a[href=?]", user_path(@user)
@@ -30,7 +29,8 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_redirected_to root_url
     follow_redirect!
     assert_select "a[href=?]", login_path
-    assert_select "a[href=?]", logout_path,      count: 0
+    assert_select "a[href=?]", logout_path, count: 0
     assert_select "a[href=?]", user_path(@user), count: 0
   end
+
 end
